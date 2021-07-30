7562e3a4906c0e1f23600065e4ab9a11f5fb984b
bellDev-code <sumaoo20@naver.com>
2021-07-30 11:43:34 +0900

    directory setting and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import LandingPage from "../src/components/views/LandingPage/LandingPage.js";
+import LoginPage from "../src/components/views/LoginPage/LoginPage.js";
+import ResisterPage from "../src/components/views/ResisterPage/Resister.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/resister" component={ResisterPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/views/ResisterPage/Resister.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Resister() {
+  return <div>Resister</div>;
+}
+
+export default Resister;
